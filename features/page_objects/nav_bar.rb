38e942d634e3b98a4c6b1bc535bee8abd773a0fd
Kathy Segura <ari.sega@gmail.com>
2024-01-28 15:00:02 -0600

    removing commented code

--- features/page_objects/nav_bar.rb
+++ features/page_objects/nav_bar.rb
@@ -3,12 +3,6 @@ class NavBar < SitePrism::Page
     element :storage_menu, "a[href='/Storage/'].show-for-large"
     element :move_in_online_today,  "a[href='/Storage/Online-Move-In/']"
 
-
-    # def click_storage_menu
-    #     wait_until_storage_menu_visible
-    #     storage_menu.hover
-    # end
-
     def click_main_menu_option(option)
         case option
         when "Storage"
@@ -16,10 +10,6 @@ class NavBar < SitePrism::Page
             storage_menu.hover
         end
     end
-
-    # def click_move_in_online_today
-    #     move_in_online_today.click
-    # end
 
     def click_on_storage_submenu(option)
       case option
